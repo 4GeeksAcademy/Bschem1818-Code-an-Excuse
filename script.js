c12ebc796da58c5d57e9ac0a345226abd951bca6
Bschem1818 <barbaraschem@gmail.com>
2024-08-09 23:02:26 +0000

    refactoring

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 window.onload = function (){
+let english = ""
 
    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
    let action = ['ate', 'peed', 'crushed', 'broke'];
    let what = ['my homework', 'my phone', 'the car'];
    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-let whoRd = Math.floor(Math.random() * who.length);
-let actionRd = Math.floor(Math.random() * action.length);
-let whatRd = Math.floor(Math.random() * what.length);
-let whenRd = Math.floor(Math.random() * when.length);
 
-document.querySelector('#excuse').innerHTML = who[whoRd] +" "+ action[actionRd] +" "+ what[whatRd] +" "+ when[whenRd];
+function grammar(a) {
+    return a [Math.floor(Math.random() * a.length)];
+}
+
+for (let i = 0; i <= 5; i++) {
+       english += `${grammar(who)} ${grammar(action)} ${grammar(what)} ${grammar(when)} <br>`;   
+
+   
+}
+
+document.querySelector('#excuse').innerHTML = english;
 
 };
